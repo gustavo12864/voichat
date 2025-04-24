97acaf57dc623e4f14175a62244590f302b63e2a
gustavo12864 <luizgustavo12865@gmail.com>
2025-04-24 17:22:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+const roomId = urlParams.get("room");
+const isJoining = urlParams.get("join") === "true";
+
+const roomCreator = document.getElementById("room-creator");
+const roomJoiner = document.getElementById("room-joiner");
+const roomLinkInput = document.getElementById("room-link");
+
 let peer;
 let conn;
+let call;
 
 function createRoom() {
   const id = Math.random().toString(36).substring(2, 10);
   const link = window.location.origin + window.location.pathname + "?room=" + id + "&join=true";
-  document.getElementById("room-creator").style.display = "none";
-  document.getElementById("room-joiner").style.display = "block";
-  document.getElementById("room-link").value = link;
+  roomCreator.style.display = "none";
+  roomJoiner.style.display = "block";
+  roomLinkInput.value = link;
 
-  startPeer(id); // Criador da sala
+  startPeer(id); // Criador da sala apenas escuta chamadas, não se conecta
 }
 
 function startPeer(id) {
@@ -42,10 +52,6 @@ function joinRoom(id) {
     });
   });
 }
-
-const urlParams = new URLSearchParams(window.location.search);
-const roomId = urlParams.get("room");
-const isJoining = urlParams.get("join") === "true";
 
 if (roomId && isJoining) {
   joinRoom(roomId);
